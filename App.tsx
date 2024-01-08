939c2b684392e41d62083963e19a3ebd6338246e
berhan10 <berhanserin7@gmail.com>
2024-01-08 20:03:32 +0300

    init

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import {View, Text, SafeAreaView} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text className="text-red-700 text-2xl">App</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
